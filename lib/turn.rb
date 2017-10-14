68b04d7744910e04c5a81627332131306959be32
Crista Cooper <aine3d@gmail.com>
2017-10-14 21:03:01 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -41,10 +41,10 @@ def move(board, index, token = "X")
 end
 
 def turn(board)
-  puts "Please enter 1 - 9"
+  puts "Please enter 1-9:"
   input = gets.strip
   until valid_move?(board, index) == true
-    puts "Please enter 1 - 9"
+    puts "Please enter 1-9:"
     input = gets.strip
     input_to_index(input)
   end
