c168d461de3f6a6b99057c60560f2058c6f5b4ed
Crista Cooper <aine3d@gmail.com>
2017-10-14 20:33:45 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -28,8 +28,6 @@ def position_taken?(board, index)
   board[index] != " " && board[index] != "" && board[index] != nil
 end
 
-
-
 def valid_move(board, index)
   if position_taken?(board, index) == true || index.between?(0, 8) == false
       return false
